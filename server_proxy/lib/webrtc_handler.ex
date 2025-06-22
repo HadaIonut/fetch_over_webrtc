8e2a160500d4d422f77c6b2c65dc0936b5ec237c
hada ionut <hadaionut@gmail.com>
2025-06-22 15:54:36 +0300

    cleanup

---
+++ server_proxy/lib/webrtc_handler.ex
@@ -0,0 +1,70 @@
+defmodule WebRTCHandler do
+  require Logger
+
+  defstruct [:peer_connection, :room_id, :user_id, :parent_pid, :decoder_pid]
+
+  def start(peer_connection, room_id, user_id, parent_pid) do
+    {:ok, _pid} =
+      Task.start(fn ->
+        loop(%WebRTCHandler{
+          peer_connection: peer_connection,
+          room_id: room_id,
+          user_id: user_id,
+          parent_pid: parent_pid
+        })
+      end)
+  end
+
+  defp loop(
+         %{
+           peer_connection: _pc,
+           room_id: room_id,
+           user_id: user_id,
+           parent_pid: parent_pid,
+           decoder_pid: decoder_pid
+         } = state
+       ) do
+    receive do
+      {:data, _data_channel, data} ->
+        Logger.warning("Received data from #{room_id} #{user_id}")
+
+        {new_state, decoder_pid} =
+          if decoder_pid == nil do
+            {:ok, decoder_pid} = WebRTCMessageDecoder.start_link(self())
+            {Map.put(state, :decoder_pid, decoder_pid), decoder_pid}
+          else
+            {state, decoder_pid}
+          end
+
+        send(decoder_pid, {:receive_message, data})
+        loop(new_state)
+
+      {:ice_candidate, candidate} ->
+        request_id = UUID.uuid4()
+
+        message =
+          JSON.encode!(%Messages.SendICE{
+            requestId: request_id,
+            roomId: room_id,
+            targetUserId: user_id,
+            iceCandidate: candidate |> ExWebRTC.ICECandidate.to_json() |> JSON.encode!()
+          })
+
+        send(parent_pid, {:send_message, message})
+
+        loop(state)
+
+      {:WebRTCDecoded, decoded} ->
+        IO.inspect(decoded)
+        loop(state)
+
+      {:ice_connection_state_change, :completed} ->
+        IO.inspect("CONNECTION ESTABLISHED")
+        loop(state)
+
+      unknown ->
+        IO.inspect("unknown message received #{inspect(unknown)}")
+        loop(state)
+    end
+  end
+end
